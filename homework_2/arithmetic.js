d5806d8a74abc54d702c6c02e6bea9d48cf6e1ab
Lex1750 <alexander.yakymchuk@gmail.com>
2023-01-16 11:16:43 +0100

    Homework_2_added

---
+++ homework_2/arithmetic.js
@@ -0,0 +1,6 @@
+alert('Cереднє арифметичне із трьох чисел:')
+let First=prompt(`Введіть число`)
+let Second=prompt(`Введіть число`)
+let Third=prompt(`Введіть число`)
+let am=(+First + +Second + +Third)
+alert(`Cереднє арифметичне ${am / 3}`)
---
+++ homework_2/calc.js
@@ -0,0 +1,8 @@
+alert('Математичні операції:')
+let FirstNumber=prompt(`Привіт,введіть число`)
+let SecondNumber=prompt(`Привіт,введіть число`)
+alert(`Додавання: ${+FirstNumber + +SecondNumber}`)
+alert(`Віднімання: ${FirstNumber - SecondNumber}`)
+alert(`Множення: ${FirstNumber * SecondNumber}`)
+alert(`Поділ: ${FirstNumber / SecondNumber}`)
+
